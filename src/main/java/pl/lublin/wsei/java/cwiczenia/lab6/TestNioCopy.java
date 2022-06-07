a9bf56bee329761c668d8450cabb6b6bfc96860d
JDrewniak2 <saylen223@wp.pl>
2022-06-07 18:14:00 +0200

    Dodanie TestNioCopy kopia obrazka z sieci

--- src/main/java/pl/lublin/wsei/java/cwiczenia/lab6/TestNioCopy.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/lab6/TestNioCopy.java
@@ -8,13 +8,13 @@ import java.nio.file.StandardCopyOption;
 
 public class TestNioCopy {
     public static void main(String[] args) throws IOException {
-        // long startTime = System.currentTimeMillis();
-        long startTime = System.nanoTime();
+         long startTime = System.currentTimeMillis();
+        //  long startTime = System.nanoTime();
         Path oryginal = Paths.get("sample_1920×1280.tiff");
         Path kopia = Paths.get("img_copy2.tiff");
         Files.copy(oryginal, kopia, StandardCopyOption.REPLACE_EXISTING);
-       // System.out.println("Czas kopiowania: " + (System.currentTimeMillis() - startTime) + " ms");
-        System.out.println("Czas kopiowania: " + (float) (System.nanoTime() - startTime) / 1000000 + " ms ");
+       System.out.println("Czas kopiowania: " + (System.currentTimeMillis() - startTime) + " ms");
+        //  System.out.println("Czas kopiowania: " + (float) (System.nanoTime() - startTime) / 1000000 + " ms ");
 
     }
 }
